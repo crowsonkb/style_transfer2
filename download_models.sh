f1f70a336875d463fc874b05e6c7153dde23b2f8
Katherine Crowson <crowsonkb@gmail.com>
2016-10-29 11:35:52 -0700

    Add a model download script

---
+++ download_models.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+url='https://style-transfer.s3-us-west-2.amazonaws.com/vgg19.caffemodel'
+
+echo 'Downloading our truncated version of the VGG-19 pre-trained model.'
+echo 'See http://www.robots.ox.ac.uk/~vgg/research/very_deep/.'
+
+cd models
+curl -L "$url" > vgg19.caffemodel.download
+mv vgg19.caffemodel.download vgg19.caffemodel
