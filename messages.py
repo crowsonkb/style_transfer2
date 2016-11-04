bae15ca8c6d48acb9f746dc4eaba88fdada57ebe
Katherine Crowson <crowsonkb@gmail.com>
2016-11-03 19:12:49 -0700

    Add optional debug logging to app/worker message creation

--- messages.py
+++ messages.py
@@ -1,9 +1,17 @@
+import inspect
+import logging
+
 import numpy as np
+
 import optimizers
+
+logger = logging.getLogger(__name__)
 
 
 class Message:
     """The base class for the messages exchanged via PyZMQ's send_pyobj()."""
+    debug = False
+
     def __repr__(self):
         def repr_value(v):
             if isinstance(v, np.ndarray):
@@ -12,6 +20,16 @@ class Message:
 
         args = ['%s=%s' % (k, repr_value(v)) for k, v in sorted(self.__dict__.items())]
         return self.__class__.__name__ + '(' + ', '.join(args) + ')'
+
+    def _debug(self):
+        if self.debug:
+            frame = inspect.currentframe()
+            try:
+                caller = frame.f_back.f_back
+                logger.debug('%s created on line %d of %s: %s', self.__class__.__name__,
+                             caller.f_lineno, caller.f_code.co_filename, repr(self))
+            finally:
+                del frame
 
 
 class Iterate(Message):
@@ -23,11 +41,13 @@ class Iterate(Message):
         self.image = image
         self.loss = loss
         self.i = i
+        self._debug()
 
 
 class PauseIteration(Message):
     """Signals the worker to pause iteration."""
-    pass
+    def __init__(self):
+        self._debug()
 
 
 class SetImages(Message):
@@ -51,6 +71,8 @@ class SetImages(Message):
         self.content_image = content_image
         self.style_image = style_image
         self.reset_state = reset_state
+        self._debug()
+
 
 class SetOptimizer(Message):
     """A request from the app to the worker to set the optimizer type and optionally step size.
@@ -65,6 +87,7 @@ class SetOptimizer(Message):
         if not step_size:
             step_size = self.step_sizes[optimizer]
         self.step_size = step_size
+        self._debug()
 
 
 class SetWeights(Message):
@@ -85,19 +108,25 @@ class SetWeights(Message):
     def __init__(self, weights, params):
         self.weights = weights
         self.params = params
+        self._debug()
 
 
 class Shutdown(Message):
     """Signals the receiving process to shut down."""
-    pass
+    def __init__(self):
+        self._debug()
+
 
 
 class StartIteration(Message):
     """Signals the worker to start iteration."""
-    pass
+    def __init__(self):
+        self._debug()
+
 
 
 class WorkerReady(Message):
     """Signals the app that the worker is ready to receive messages and should be initialized."""
     def __init__(self, send_images=False):
         self.send_images = send_images
+        self._debug()
