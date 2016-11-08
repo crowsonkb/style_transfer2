e1202385f6ddac7ace23b2a34a02287cc9930699
Katherine Crowson <crowsonkb@gmail.com>
2016-11-08 11:19:48 -0800

    Docstrings

--- error_pages.py
+++ error_pages.py
@@ -1,3 +1,5 @@
+"""aiohttp middleware to serve HTTP error pages as jinja2 templates."""
+
 from aiohttp import web
 import aiohttp_jinja2
 
@@ -7,22 +9,30 @@ MESSAGES = {
     or come back in about a minute, it might be available.''',
 }
 
+TEMPLATE = 'error.html'
+
 
 class ErrorPages:
+    """aiohttp middleware to serve HTTP error pages as jinja2 templates."""
+
     async def __call__(self, app, handler):
-        async def middleware_handler(request):
+        """Middleware factory method."""
+
+        async def error_handler(request):
+            """Handler to serve HTTP error pages as jinja2 templates."""
+
             try:
                 response = await handler(request)
             except web.HTTPException as err:
                 response = err
             if response.status >= 400:
                 context = dict(status_code=response.status,
-                            reason=response.reason,
-                            message=MESSAGES.get(response.status, ''))
+                               reason=response.reason,
+                               message=MESSAGES.get(response.status, ''))
 
-                @aiohttp_jinja2.template('error.html', status=response.status)
+                @aiohttp_jinja2.template(TEMPLATE, status=response.status)
                 async def template_fn(request):
                     return context
                 return await template_fn(request)
             return response
-        return middleware_handler
+        return error_handler
